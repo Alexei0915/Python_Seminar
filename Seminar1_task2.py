c27e343112dbda9ddb87a2d3c3a0d776abecbb62
Alexei <vertuginalexei7@gmail.com>
2022-09-26 15:02:38 +0300

    Task2

---
+++ Seminar1_task2.py
@@ -0,0 +1,9 @@
+# Нпишите программу которая принимает пять чисел и находит максимальное из этих чисел .
+
+list = [3, 2, 8, 5, 10, 6]
+max_number = max(list)
+print("Наибольшее число:", max_number)
+# Или такой вариант )
+# from functools import reduce
+# list1 = [-1, 3, 45, 9998, 0]
+# print(reduce(max, list1))  
