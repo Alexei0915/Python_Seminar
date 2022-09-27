6c7fd3b03fda4c06f841c0ffdf798077de8172fd
Alexei <vertuginalexei7@gmail.com>
2022-09-27 13:24:50 +0300

    DZ5

--- dz_task3.py
+++ dz_task3.py
@@ -25,3 +25,15 @@ def quarter(x,y):
     print(f"Точка находится в {a} четверти плоскости")
 
 quarter(x,y)
+
+# x = int(input('Введите номер четверти от 1 до 4  '))
+# if x==1: 
+#     print('x>0 and y>0')
+# elif x==2:
+#      print('x<0 and y>0')
+# elif x==3:
+#      print('x<0 and y<0')
+# elif x==4 : 
+#     print('x>0 and y<0')
+# else: 
+#     print('Введите корректное значение')
