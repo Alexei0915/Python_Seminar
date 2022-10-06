30624990c0b641852519d2d49377ffee6c03ab88
Alexei <vertuginalexei7@gmail.com>
2022-10-06 14:25:28 +0300

    Task5

--- dz3_task5.py
+++ dz3_task5.py
@@ -1,17 +1,27 @@
 # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 # Пример:
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
-k = int(input('Введите целое число : ')) 
-k1 = 0
-k2 = 1
-if k <= 0:
-    k = input('Введите целое число > 0: ')
-elif k == 1:
-    print(k1)
-elif k == 2:
-    print(k2)
-else:
-    print(0, 1, end=' ')
-    for i in range(2, k):
-        k1, k2 = k2, k1 + k2
-        print(k2, end=' ')
+
+def Fibonacci(n):
+    if n in [1, 2]:                       
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
+def NFibonacci(n):
+    if n == 1:                       
+        return 1
+    elif n == 2:                       
+        return -1
+    else:
+        num1, num2 = 1, -1
+        for i in range(2, n):
+            num1, num2 = num2, num1 - num2
+        return num2
+
+list = [0]
+uNum = int(input('Введите число: '))
+for e in range(1, uNum + 1):
+    list.append(Fibonacci(e))
+    list.insert(0, NFibonacci(e))
+print(list)
